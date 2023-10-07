ca6b872d2ba1f8c3540da0b670c13410b4d48bba
MatheDevGr <matheus.reis@modalgr.com.br>
2023-10-07 18:45:58 -0300

    init: initial comit

---
+++ TravelApp/Views/CelulaViagemView.swift
@@ -0,0 +1,18 @@
+//
+//  CelulaViagemView.swift
+//  TravelApp
+//
+//  Created by Matheus Reis on 07/10/23.
+//
+
+import SwiftUI
+
+struct CelulaViagemView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CelulaViagemView()
+}
---
+++ TravelApp/Views/HeaderView.swift
@@ -0,0 +1,18 @@
+//
+//  HeaderView.swift
+//  TravelApp
+//
+//  Created by Matheus Reis on 07/10/23.
+//
+
+import SwiftUI
+
+struct HeaderView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HeaderView()
+}
