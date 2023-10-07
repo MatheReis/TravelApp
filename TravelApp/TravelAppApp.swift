e6e8af057f4c072d8cdcf86b68251902080547ee
MatheDevGr <matheus.reis@modalgr.com.br>
2023-10-07 10:40:17 -0300

    Initial Commit

---
+++ TravelApp/TravelAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravelAppApp.swift
+//  TravelApp
+//
+//  Created by Matheus Reis on 07/10/23.
+//
+
+import SwiftUI
+
+@main
+struct TravelAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
